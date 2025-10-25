0956bc230db66f8fabd70eb2d769d8e8704343cc
Sampath-04 <sampathkaali002@gmail.com>
2025-10-25 20:07:30 +0530

    local host is used in backend

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,7 +11,7 @@ import {
   TaskPriority,
 } from '@/types/task';
 
-const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/tasks` || 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8080/api/v1/tasks';
 
 // Create axios instance with default config
 const apiClient = axios.create({
